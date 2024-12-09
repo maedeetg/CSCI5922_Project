ac19c16c3ebfcfe1219aec4bcaae057ae82b4c1a
Maedee Trank-Greene <matr4909@colorado.edu>
2024-12-08 22:46:42 -0700

    Fix bugs

--- DataCleaning.R
+++ DataCleaning.R
@@ -3,21 +3,14 @@ library(dplyr)
 
 # Read in data
 songs <- read.csv("TheBeatlesSongs.csv") 
-songs <- songs %>% select(-mode)
 songs_john_paul <- songs %>% filter(vocals == "John" | vocals == "Paul")
 songs_john_paul <- songs_john_paul %>% mutate(across(c(liveness, valence), as.numeric))
 
 # Make labels numeric, Paul = 1, John = 0
-songs_0_1 <- songs_john_paul_norm %>% mutate(vocals = ifelse(songs_john_paul$vocals == "Paul", 1, 0))
+songs_0_1 <- songs_john_paul %>% mutate(vocals = ifelse(songs_john_paul$vocals == "Paul", 1, 0))
 
 # Normalize data
-songs_john_paul_norm <- songs_john_paul %>% mutate(danceability = (danceability - mean(danceability))/sd(danceability),
-                                                   energy = (energy - mean(energy))/sd(energy),
-                                                   speechiness = (speechiness - mean(speechiness))/sd(speechiness),
-                                                   acousticness = (acousticness - mean(acousticness))/sd(acousticness),
-                                                   liveness = (liveness - mean(liveness))/sd(liveness),
-                                                   valence = (valence - mean(valence))/sd(valence),
-                                                   duration_ms = (duration_ms - mean(duration_ms))/sd(duration_ms))
+songs_john_paul_norm <- songs_john_paul %>% mutate(duration_ms = (duration_ms - min(duration_ms))/(max(duration_ms) - min(duration_ms)))
 
 # Training and testing data for entire dataset
 set.seed(123)
@@ -31,9 +24,9 @@ older_years_train <- train %>% filter(year > 1966)
 older_years_test <- test %>% filter(year > 1966)
 
 # Export as csv to be used in python
-write.csv(train, "train.csv", row.names = TRUE)
-write.csv(test, "train.csv", row.names = TRUE)
-write.csv(early_years_train, "train.csv", row.names = TRUE)
-write.csv(early_years_test, "train.csv", row.names = TRUE)
-write.csv(older_years_train, "train.csv", row.names = TRUE)
-write.csv(older_years_test, "train.csv", row.names = TRUE)
+write.csv(train, "train.csv", row.names = FALSE, fileEncoding = "UTF-8" )
+write.csv(test, "test.csv", row.names = FALSE, fileEncoding = "UTF-8" )
+write.csv(early_years_train, "early_train.csv", row.names = FALSE, fileEncoding = "UTF-8")
+write.csv(early_years_test, "early_test.csv", row.names = FALSE, fileEncoding = "UTF-8" )
+write.csv(older_years_train, "older_train.csv", row.names = FALSE, fileEncoding = "UTF-8" )
+write.csv(older_years_test, "older_test.csv", row.names = FALSE, fileEncoding = "UTF-8" )
