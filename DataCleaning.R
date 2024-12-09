623c4671894acb69a1aaad838f1c1d0288676344
Maedee Trank-Greene <matr4909@colorado.edu>
2024-12-09 00:18:10 -0700

    Normalize

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,10 +1,11 @@
 # Load in libraries
 library(dplyr)
+library(tidyverse)
 
 # Read in data
 songs <- read.csv("TheBeatlesSongs.csv") 
 songs_john_paul <- songs %>% filter(vocals == "John" | vocals == "Paul")
-songs_john_paul <- songs_john_paul %>% mutate(across(c(liveness, valence), as.numeric))
+songs_john_paul <- songs_john_paul %>% mutate(across(c(liveness, valence), as.numeric)) %>% select(-mode)
 
 # Make labels numeric, Paul = 1, John = 0
 songs_0_1 <- songs_john_paul %>% mutate(vocals = ifelse(songs_john_paul$vocals == "Paul", 1, 0))
@@ -14,8 +15,8 @@ songs_john_paul_norm <- songs_john_paul %>% mutate(duration_ms = (duration_ms - 
 
 # Training and testing data for entire dataset
 set.seed(123)
-train <- songs_0_1 %>% sample_n(floor(nrow(songs_0_1)*0.8), replace = FALSE)
-test <- anti_join(songs_0_1, train, by = colnames(subset))
+train <- songs_john_paul_norm %>% sample_n(floor(nrow(songs_john_paul_norm)*0.8), replace = FALSE)
+test <- anti_join(songs_john_paul_norm, train, by = colnames(subset))
 
 # Make early years and older years
 early_years_train <- train %>% filter(year <= 1966)
@@ -30,3 +31,4 @@ write.csv(early_years_train, "early_train.csv", row.names = FALSE, fileEncoding 
 write.csv(early_years_test, "early_test.csv", row.names = FALSE, fileEncoding = "UTF-8" )
 write.csv(older_years_train, "older_train.csv", row.names = FALSE, fileEncoding = "UTF-8" )
 write.csv(older_years_test, "older_test.csv", row.names = FALSE, fileEncoding = "UTF-8" )
+
